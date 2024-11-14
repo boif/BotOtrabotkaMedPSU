0ae51f58e0f500aceb416c9a3eb245108808f37e
boif <chlentim7@gmail.com>
2024-11-15 00:31:17 +0300

    little fix

--- src/vk_monitor.py
+++ src/vk_monitor.py
@@ -2,7 +2,7 @@ import requests
 import re
 
 ACCESS_TOKEN = "vk1.a.ZAbDb84oF69OQ7p23CF_VYDD2GdqOodFz7yqYKiOZWhJsc6syBFWVCWQFLtxByPkSzmJbpj5ulqZHkjf2rMJc6BM9mLHd78AK_yJXZQ0xlu4E5lRwx39YCorVE5oDIGyXuzwDQYRcR-agXXpkPtZS80GeNQkake4k__NZDsazuqW0YmSrdJnuQ6Dg8wXJZOOeifBRZ69FYYTN1nCK7rTHw&expires_in=86400"
-OWNER_ID = "postoyuvstoronke"
+OWNER_ID = "boif3x"
 
 
 def get_latest_post():
